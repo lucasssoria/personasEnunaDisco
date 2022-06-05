2757762ca693b136e54c904992e9b82e246ad3c3
irina-marzioni-tdf <104698059+irina-marzioni-tdf@users.noreply.github.com>
2022-06-05 11:32:03 -0300

    initial commit

--- src/index.ts
+++ src/index.ts
@@ -1,19 +1,21 @@
 let ingresos: number[] = new Array(0);
-let mayoroigual21: number[] = new Array(0);
-let menores21: number[] = new Array(0);
+let mayorOIgual21: number = 0;
+let menores21: number = 0;
 let min = 18;
 let max = 40;
+
 for (let i = 0; i < 270; i++) {
   ingresos[i] = Math.floor(Math.random() * (max - min) + min);
   if (ingresos[i] >= 21) {
-    mayoroigual21[i] = ingresos[i];
-  } else if (ingresos[i] < 21) {
-    menores21[i] = ingresos[i];
+    mayorOIgual21 = mayorOIgual21 + 1;
+  } else {
+    menores21 = menores21 + 1;
   }
 }
-console.log(mayoroigual21);
-console.log(menores21);
-console.log(ingresos);
+
+console.log("Personas mayores a 21 ", mayorOIgual21);
+console.log("Personas menores a 21", menores21);
+console.log("Total de personas", ingresos.length);
 
 // arreglos usando filter
 // let menores21 = ingresos.filter((ingresos) => ingresos < 21);
